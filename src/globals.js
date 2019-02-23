cd01688ffa473098d5bbe00e6662382b87af060a
trentv4 <trentvanslyke@gmail.com>
2019-02-22 23:38:04 -0500

    Add the 'add', 'remove', 'list' commands for simple responses

--- src/globals.js
+++ src/globals.js
@@ -3,8 +3,17 @@ console.write = (message) => {
 	process.stdout.write(message);
 }
 
+Math.randomS = (length) => {
+	return Math.floor(Math.random() * length)
+}
+
+ROLE_ANTI_SHITPOST = { id: 309851110829457410, level: 1}
+ROLE_MOD_ABUSE = { id: 332707439906390018, level: 2}
+ROLE_SERVER_ADMINS = { id: 263762720719896578, level: 3}
+
 // Gets the nickname if available, or defaults to username if not.
 getUsername = (message) => {
+	if(message.member == null) return "Unknown :shrug:"
 	return message.member.nickname == null ? message.author.username : message.member.nickname;
 }
 
@@ -12,17 +21,15 @@ isSymbolCommandTrigger = (symbol) => {
 	return (symbol == "=")
 }
 
-getPermissionLevel = (userID, guild) => {
-	let user = guild.members.get(userID)
+getPermissionLevel = (user) => {
 	let roles = user.roles.array()
-	let permLevel = 0
-	roles.forEach(e => {
-		if(e.id == 480216395205050368 || e.name == "mod" || e.name == "Jake") {
-			permLevel = 1
-		}
-	})
-
-	return permLevel
+	let maxPerm = 0
+	for(let i = 0; i < roles.length; i++) {
+		if(roles[i].id == ROLE_ANTI_SHITPOST.id && maxPerm < ROLE_ANTI_SHITPOST.level) maxPerm = ROLE_ANTI_SHITPOST.level
+		if(roles[i].id == ROLE_MOD_ABUSE.id && maxPerm < ROLE_MOD_ABUSE.level) maxPerm = ROLE_MOD_ABUSE.level
+		if(roles[i].id == ROLE_SERVER_ADMINS.id && maxPerm < ROLE_SERVER_ADMINS.level) maxPerm = ROLE_SERVER_ADMINS.level
+	}
+	return maxPerm
 }
 
 META_GENERAL = {
@@ -55,10 +62,44 @@ META_MARKOV_TAYTAY = {
 	permissions: 0
 }
 
-META_MODERATOR = {
+META_RESPONSES = {
 	hidden: false,
-	category: "moderation",
-	permissions: 1
+	category: "responses",
+	permissions: 0
 }
 
-messageBuffer = []
+META_DEBUG = {
+	hidden: true,
+	category: "debug",
+	permissions: 0	
+}
+
+commandList = {
+	commands: {
+		meta: META_GENERAL,
+		execute: (commands, message) => {
+			let cmdList = {}
+			Object.keys(commandList).forEach(command => {
+				let actualCommand = commandList[command]
+
+				if(actualCommand.meta.hidden == true) 
+					return
+				if(cmdList[actualCommand.meta.category] == undefined) 
+					cmdList[actualCommand.meta.category] = []
+
+				cmdList[actualCommand.meta.category].push(command)
+			})
+
+			let output = ""
+
+			Object.keys(cmdList).forEach(cat => {
+				output += "**" + cat + "**: \n"
+				Object.keys(cmdList[cat]).forEach(command => {
+					output += cmdList[cat][command] + ", "
+				})
+				output = output.substring(0, output.length-2) + "\n"
+			})
+			message.channel.send(output)
+		}
+	}
+}
--- src/main.js
+++ src/main.js
@@ -2,37 +2,6 @@ require(__dirname + "/globals.js")
 const fs = require("fs")
 const client = new (require("discord.js")).Client( { autoReconnect: true } );
 
-// This is the global commandList that other commands are added to.
-const commandList = {
-	commands: {
-		meta: META_GENERAL,
-		execute: (commands, message) => {
-			let cmdList = {}
-			Object.keys(commandList).forEach(command => {
-				let actualCommand = commandList[command]
-
-				if(actualCommand.meta.hidden == true) 
-					return
-				if(cmdList[actualCommand.meta.category] == undefined) 
-					cmdList[actualCommand.meta.category] = []
-
-				cmdList[actualCommand.meta.category].push(command)
-			})
-
-			let output = ""
-
-			Object.keys(cmdList).forEach(cat => {
-				output += "**" + cat + "**: \n"
-				Object.keys(cmdList[cat]).forEach(command => {
-					output += cmdList[cat][command] + ", "
-				})
-				output = output.substring(0, output.length-2) + "\n"
-			})
-			message.channel.send(output)
-		}
-	}
-}
-
 // Loads from an external file that contains specific commands.
 function apply(target) {
 	let methods = Object.keys(target)
@@ -42,15 +11,6 @@ function apply(target) {
 }
 
 function getCommand(raw) {
-	/*
-	if(raw.content.substring(0, 42).toLowerCase() == "i knew you were trouble when you logged in") {
-		return commandList["kick"]
-	}
-	if(raw.content.substring(0, 46).toLowerCase() == "we are never, ever, ever getting back together") {
-		return commandList["ban"]
-	}
-	*/
-
 	let splitCommand = raw.content.substring(1, raw.content.length).split(" ")
 
 	if(isSymbolCommandTrigger(raw.content[0])) {
@@ -64,7 +24,6 @@ function getCommand(raw) {
 
 apply(require(__dirname + "/markov.js"))
 apply(require(__dirname + "/responses.js"))
-//apply(require(__dirname + "/moderation.js"))
 
 console.write("Connecting... ")
 
